7e85576e8b627d367d56ed12a90bac18b50be7e1
David Mihal <eastsideski@gmail.com>
2015-12-13 20:43:57 -0500

    Add remaining code

--- src/scraper.js
+++ src/scraper.js
@@ -1,3 +1,4 @@
+zip.useWebWorkers=false;
 zip.createWriter(new zip.BlobWriter("application/zip"), function(zipWriter) {
   var thumbs = document.querySelectorAll('.uiMediaThumbMedium');
   var imagePromises = [];
@@ -17,7 +18,28 @@ zip.createWriter(new zip.BlobWriter("application/zip"), function(zipWriter) {
       var url = window.URL.createObjectURL(blob);
       console.log("Zip Download:", url);
     });
-  })
+  });
 }, function(err){
   console.error(err);
-}));
+});
+
+function urlToDataUrl(url){
+  var promise = new Promise(function(resolve, reject){
+    var img = new Image();
+    img.crossOrigin = "anonymous"
+    img.src = url;
+    img.addEventListener('load', function(){
+      var dataUrl = getBase64Image(img);
+      resolve(dataUrl);
+    });
+  });
+  function getBase64Image(img) {
+      var canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      return canvas.toDataURL("image/png");
+  }
+  return promise;
+}
