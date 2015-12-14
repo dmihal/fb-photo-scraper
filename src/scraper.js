2e3bc3e6901d79801c0c284a0b9f5398c87899c5
David Mihal <eastsideski@gmail.com>
2015-12-13 20:24:52 -0500

    Write most of scraper structure

--- src/scraper.js
+++ src/scraper.js
@@ -1,6 +1,23 @@
-var thumbs = document.querySelectorAll('.uiMediaThumbMedium');
-for (var i=0; i<thumbs.length; i++) {
-  var ajaxify_str = decodeURIComponent(thumbs[i].attributes.getNamedItem('ajaxify').value);
-  var url = ajaxify_str.substr(ajaxify_str.indexOf('src=')+4);
-  console.log(url);
-}
+zip.createWriter(new zip.BlobWriter("application/zip"), function(zipWriter) {
+  var thumbs = document.querySelectorAll('.uiMediaThumbMedium');
+  var imagePromises = [];
+  for (var i=0; i<thumbs.length; i++) {
+    var ajaxify_str = decodeURIComponent(thumbs[i].attributes.getNamedItem('ajaxify').value);
+    var url = ajaxify_str.substr(ajaxify_str.indexOf('src=')+4);
+    imagePromises.push(new Promise(function(resolve, reject){
+      urlToDataUrl(url).then(function(dataUrl){
+        zipWriter.add("filename", new zip.Data64URIReader(dataUrl), function(){
+          resolve();
+        });
+      });
+    }));
+  }
+  Promise.all(imagePromises).then(function(){
+    zipWriter.close(function(blob){
+      var url = window.URL.createObjectURL(blob);
+      console.log("Zip Download:", url);
+    });
+  })
+}, function(err){
+  console.error(err);
+}));
